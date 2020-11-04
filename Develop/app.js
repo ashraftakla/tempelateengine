1aab73ac2c4a9c6c08c4497c68974d4bc8b51ae7
Ashraf Takla <ashraf.takla@icloud.com>
2020-11-04 16:15:51 -0500

    homework is fully functional

--- Develop/app.js
+++ Develop/app.js
@@ -9,6 +9,132 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
+const { CLIENT_RENEG_LIMIT } = require("tls");
+
+let allEmployees = [];
+
+function writeToTemplate() {
+    const templateFile = render(allEmployees);
+    fs.writeFile(outputPath, templateFile, function (err) {
+        if (err) throw err;
+        console.log('Saved!');
+    });
+}
+function managerQuestion() {
+    inquirer.prompt([{
+        name: "name",
+        type: "input",
+        message: "What is the manger's name?",
+    }, {
+        name: "id",
+        type: "input",
+        message: "What is the manger's id?",
+    }, {
+        name: "email",
+        type: "input",
+        message: "What is the manger's email?",
+    }, {
+        name: "officeNumber",
+        type: "input",
+        message: "What is the manger's office number?",
+    },]).then(answers => {
+        let manager = new Manager(answers.name, answers.id, answers.email, answers.officeNumber);
+        allEmployees.push(manager);
+        console.log(allEmployees);
+        init();
+    }).catch(error => {
+        if (error) throw error;
+    });
+}
+
+function engineerQuestion() {
+    inquirer.prompt([{
+        name: "name",
+        type: "input",
+        message: "What is the engineer's name?",
+    }, {
+        name: "id",
+        type: "input",
+        message: "What is the engineer's id?",
+    }, {
+        name: "email",
+        type: "input",
+        message: "What is the engineer's email?",
+    }, {
+        name: "github",
+        type: "input",
+        message: "What is the engineer's github account?",
+    },]).then(answers => {
+        let engineer = new Engineer(answers.name, answers.id, answers.email, answers.github);
+        allEmployees.push(engineer);
+        console.log(allEmployees);
+        init();
+    }).catch(error => {
+        if (error) throw error;
+    });
+}
+
+function internQuestion() {
+    inquirer.prompt([{
+        name: "name",
+        type: "input",
+        message: "What is the intern's name?",
+    }, {
+        name: "id",
+        type: "input",
+        message: "What is the intern's id?",
+    }, {
+        name: "email",
+        type: "input",
+        message: "What is the intern's email?",
+    }, {
+        name: "school",
+        type: "input",
+        message: "What is the intern's school?",
+    },]).then(answers => {
+        let intern = new Intern(answers.name, answers.id, answers.email, answers.school);
+        allEmployees.push(intern);
+        console.log(allEmployees);
+        init();
+    }).catch(error => {
+        if (error) throw error;
+    });
+
+}
+function init() {
+    inquirer.prompt({
+        name: "typeOfEmployee",
+        type: "list",
+        message: "What kind of employee would you like to enter next?",
+        choices: ["Manager", "Engineer", "Intern", "No more Employees"]
+    }).then(answers => {
+        console.log(answers);
+        switch (answers.typeOfEmployee) {
+            case "Manager":
+                managerQuestion();
+
+                break;
+            case "Engineer":
+                engineerQuestion();
+
+                break;
+            case "Intern":
+                internQuestion();
+                break;
+            default:
+                writeToTemplate();
+                console.log("You are done entering employees!");
+                break;
+
+
+        }
+
+    }).catch(error => {
+        if (error) throw error;
+    })
+}
+init();
+
 
 
 // Write code to use inquirer to gather information about the development team members,
